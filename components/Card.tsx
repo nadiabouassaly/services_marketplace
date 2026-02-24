ad332a5a2194061eb789b115669afccdc3b843f6
giorgiokfoury <gmk25@mail.aub.edu>
2026-02-24 14:04:01 +0200

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,10 +8,10 @@ type CardProps = {
 export default function Card({ name, description, price}: CardProps) {
   return (
     
-    <div className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow">
+    <div className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow flex flex-col">
       <h3 className="text-lg font-bold mb-2">{name}</h3>
       <p className="text-gray-700">{description}</p>
-      <p className="mt-2 text-green-600 font-bold">${price}</p>
+      <p className="mt-auto text-right text-green-600 font-bold">${price}</p>
     </div>
   );
 }
