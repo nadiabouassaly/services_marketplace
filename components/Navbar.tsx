691531a3fad5d6b8adbe81a16e5cf6a846d58861
Nadia Abou Assaly <nadiabouassaly@gmail.com>
2026-03-13 21:34:15 +0200

    added conditions for the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,16 +5,32 @@ import NavbarButton from './NavbarButton';
 import ProfileIcon from './ProfileIcon';
 import { FaSearch } from 'react-icons/fa';
 import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
   const router = useRouter();
+  const pathName = usePathname();
+  
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
         <Logo />
 
         {/* Center Search bar */}
-        <div className="flex-1 mx-6 relative max-w-md">
+        {(pathName == "/") && <SearchBar/>}
+
+        <div className="flex items-center space-x-4">
+          <ProfileIcon />
+          {(pathName == "/") && <NavbarButton onClick={()=>router.push("/Post")}>+ Post a Service</NavbarButton>}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function SearchBar(){
+  return (
+    <div className="flex-1 mx-6 relative max-w-md">
           <FaSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-500 text-sm" />
           <input
             type="text"
@@ -22,12 +38,5 @@ export default function Navbar() {
             className="w-full pl-11 pr-6 py-2 rounded-md bg-gray-100 text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#0a74ff]"
           />
         </div>
-
-        <div className="flex items-center space-x-4">
-          <ProfileIcon />
-          <NavbarButton onClick={()=>router.push("/Post")}>+ Post a Service</NavbarButton>
-        </div>
-      </div>
-    </nav>
   );
 }
