ba39722942ba8049c8dfe951c2f48ceee3baa6b8
Nadia Abou Assaly <nadiabouassaly@gmail.com>
2026-03-13 21:42:32 +0200

    .

--- app/Post/page.tsx
+++ app/Post/page.tsx
@@ -1,6 +1,12 @@
 
 export default function Post(){
     return(
-        
+        <>
+            <div style={{textAlign: "center"}}>
+                <button style={{marginRight: 10}}>prev</button>
+                <button>1/100</button>
+                <button style={{marginLeft:10}}>next</button>
+            </div>
+        </>
     );
 }
