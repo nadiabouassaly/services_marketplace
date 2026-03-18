4ef41b580d46ca60b890adf3e2560a4cb4c6c280
Nadia Abou Assaly <nadiabouassaly@gmail.com>
2026-03-18 12:59:27 +0200

    .

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -65,7 +65,6 @@ export default function Pagination(numOfPages: ButtonProps){
 
     }, [searchParams]);
 
-    //next page
     const nextPage = () =>{
 
         if(currentPage != numOfPages.props){
